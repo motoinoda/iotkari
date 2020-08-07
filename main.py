ffe2256103c5934b9e81a2107d07c6cb8b5b89e2
motoinoda <motoinodax@gmail.com>
2020-08-07 21:39:46 +0900

    ok

--- main.py
+++ main.py
@@ -66,13 +66,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ledをつけるよ'))
 
     msg = event.message.text.encode('utf-8')
     if msg in f_msg:
-	    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ledをつけるよ'))
         publish_aircon_control_msg('on')
     elif msg in b_msg:
-	    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ledを消すよ'))
+	    # line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ledを消すよ'))
         publish_aircon_control_msg('off')
     elif msg in l_msg:
         publish_aircon_control_msg('A')
