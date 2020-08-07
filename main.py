c78c6061ca2c431e24ce7b8cf7256b92a4a52f2e
motoinoda <motoinodax@gmail.com>
2020-08-07 14:59:03 +0900

    ok

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text+'aa'))
 
     TextSendMessage(text="あいうえお")
 
